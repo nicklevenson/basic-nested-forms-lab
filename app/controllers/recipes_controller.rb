ebb476406a8f3f6a91420b21caa0304d790c5055
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-21 10:46:43 -0800

    commit tree 4ed74dd3e9dfa9377e56cdd3c9388306254a30b3

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+
   end
 
   def index
@@ -9,8 +10,18 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'name', quantity: 'quantity')
+    @recipe.ingredients.build(name: 'name', quantity: 'quantity')
   end
 
   def create
+    Recipe.create(recipe_params)
+  end
+
+  private
+
+  def recipe_params
+
+    params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity])
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,9 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients 
+
+  # def create(recipe, ingredients)
+  #   recipe = Recipe.create(recipe)
+  #   recipe.ingredients.build(ingredients)
+  # end
 end
